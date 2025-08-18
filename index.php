a817e5190d087e0a82671cb625004d5e7e2228e1
Pavel Sherer <pavel@sherer.pro>
2025-08-18 18:17:13 +0300

    Test, #deploy

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 /**
+ * Подключение автозагрузчика Composer с проверкой наличия.
+ *
+ * @throws RuntimeException если autoload отсутствует.
+ */
+$autoload = __DIR__ . '/vendor/autoload.php';
+if (!file_exists($autoload)) {
+    logError('Не найден autoload Composer. Выполни "composer install".');
+    throw new RuntimeException('Файл autoload.php отсутствует');
+}
+require_once $autoload;
+
+/**
  * Точка входа для HTTP-запросов к корню домена.
  * Просто делегирует обработку webhook-скрипту бота.
  */
